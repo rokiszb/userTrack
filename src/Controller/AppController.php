591707246c90fb1f5fcd260b6a1d49351b5a8d20
rokas <rokas.lakstauskas@gmail.com>
2021-04-12 23:54:29 +0300

    updates

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,19 +2,32 @@
 
 namespace App\Controller;
 
+use App\Form\TaskType;
+use App\Repository\TaskListRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AppController extends AbstractController
 {
+    private $taskListRepository;
+
+    public function __construct(
+        TaskListRepository $taskListRepository
+    )
+    {
+        $this->taskListRepository = $taskListRepository;
+    }
     /**
-     * @Route("/list", name="list")
+     * @Route("/list", name="list_view")
      */
     public function index(): Response
     {
-        return $this->render('app/index.html.twig', [
-            'controller_name' => 'AppController',
+        $list = $this->taskListRepository->findOneBy(['user' => $this->getUser()]);
+        $form = $this->createForm(TaskType::class);
+        dump($list);
+        return $this->render('app/list/index.html.twig', [
+            'task' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/TaskType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Task;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('comment', TextType::class)
+            ->add('date', DateTimeType::class)
+            ->add('timeSpent', NumberType::class, [
+
+            ])
+
+            ->add('submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Task::class,
+        ]);
+    }
+}
